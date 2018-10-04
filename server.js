723179deec8b8b20d798d90961823c5b2be0c9c9
unknown <msvendsentan@gmail.com>
2018-10-04 18:17:45 -0400

    port fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const api_routes = require("./routes/api_routes.js");
 // Require all models
 //const db = require("./models");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
